152a2fdd105fc9fef9e83deed7812e71bccb9494
nithin varma <nithinvarma411@gmail.com>
2025-03-12 13:43:44 +0530

    Update auth.middleware.js

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,7 +4,7 @@ import { User } from '../models/user.model.js';
 export const verifyJWT = async (req, res, next) => {
     
     const token = req.header("Authorization")?.replace("Bearer ", "");
-    console.log(token);
+    // console.log(token);
 
     if (!token) {
         return res.status(401).json({
@@ -18,7 +18,7 @@ export const verifyJWT = async (req, res, next) => {
         // console.log(decodedToken)
 
         const user = await User.findById(decodedToken._id).select("-password -refreshToken");
-        console.log("user", user)
+        // console.log("user", user)
 
         if (!user) {
             return res.status(401).json({
